c4329ddb6179d4d7353d3969e09a0c90ffdd7eec
Jack <zxcqwe490649064906@gmail.com>
2023-04-09 09:28:25 +0800

    [issue #1] Support add three values
    - The third value is optional.
    - Add test code.

--- simple_calculate_class.py
+++ simple_calculate_class.py
@@ -1,6 +1,6 @@
 class Calculate:
-    def add(self, a, b):
-        return a + b
+    def add(self, a, b, c=0):
+        return a + b + c
 
     def sub(self, a, b):
         return a - b
@@ -9,3 +9,4 @@ if __name__ == '__main__':
     c = Calculate()
     print(f'5 + 8 = {c.add(5, 8)}')
     print(f'5 - 8 = {c.sub(5, 8)}')
+    print(f'5 + 8 + 1 = {c.add(5, 8, 1)}')
