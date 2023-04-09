197e38847b3361935e9dea03075eeb0714f5667d
Jack <zxcqwe490649064906@gmail.com>
2023-04-09 09:36:08 +0800

    [Add debug flag]

--- simple_calculate_class.py
+++ simple_calculate_class.py
@@ -1,11 +1,16 @@
 class Calculate:
-    def add(self, a: int, b: int) -> int:
+    def add(self, a: int, b: int, debug=False) -> int:
+        if debug:
+            print(f'a: {a}, b: {b}')
         return a + b
 
-    def sub(self, a: int, b: int) -> int:
+    def sub(self, a: int, b: int, debug=False) -> int:
+        if debug:
+            print(f'a: {a}, b: {b}')
         return a - b
 
 if __name__ == '__main__':
     c = Calculate()
     print(f'5 + 8 = {c.add(5, 8)}')
     print(f'5 - 8 = {c.sub(5, 8)}')
+    print(f'5 + 8 = {c.add(5, 8, debug=True)}')
